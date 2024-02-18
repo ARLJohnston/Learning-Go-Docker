b8c5cbbae02558d0581af00f56e1ca582c5e2e00
ARLJohnston <40921666+ARLJohnston@users.noreply.github.com>
2024-02-18 18:07:50 +0000

    test: add test for greeting people or world

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-  got := hello()
-  want := "Hello, world!"
+  t.Run("Saying hello to myself", func(t *testing.T) {
+    got := hello("Alistair")
+    want := "Hello, Alistair!"
 
-  if got != want {
-	  t.Errorf("got %q want %q", got, want)
-  }
+    if got != want {
+            t.Errorf("got %q want %q", got, want)
+    }
+  })
+
+
+  t.Run("Saying hello world when no one answers", func(t *testing.T) {
+    got := hello("")
+    want := "Hello, World!"
+
+    if got != want {
+            t.Errorf("got %q want %q", got, want)
+    }
+  })
 }
